98489b1b741e440043dbc2e0d41444546c84e4c8
git-sergBond <git-serg.bond@yandex.ru>
2022-05-03 05:14:12 +0300

    исправил Раннее время начала

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,37 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        Var2Ex1();
+        Example1();
+        //Var2Ex1();
+    }
+
+    /**
+     * Пример 1.
+     */
+    public static void Example1() {
+        Node n1 = Node.createInitialNode();
+        Node n2 = new Node(10);
+        Node n3 = new Node(5);
+        Node n4 = new Node(7);
+        Node n5 = new Node(6);
+        Node n6 = new Node(8);
+        Node n7 = new Node(5);
+        Node n8 = new Node(10);
+        Node n9 = new Node(5);
+        Node n10 =  Node.createFinishNode();
+        //TODO написать в дальнейшем прогу для вычисления предшественниов и последователей
+        n1.addNext(n2);
+        n2.addPre(n1)           .addNext(n3,n5);
+        n3.addPre(n2)           .addNext(n4);
+        n4.addPre(n3, n5)           .addNext(n7);
+        n5.addPre(n2)           .addNext(n4,n6);
+        n6.addPre(n5)           .addNext(n7);
+        n7.addPre(n4,n6)       .addNext(n8, n9);
+        n8.addPre(n7)       .addNext(n10);
+        n9.addPre(n7)   .addNext(n10);
+        n10.addPre(n8,n9);
+
+        networkPlaningModel(Arrays.asList(n1, n2, n3, n4, n5, n6, n7, n8, n9, n10));
     }
 
     /**
@@ -51,9 +81,11 @@ public class Main {
         //TODO + тут важен порядок вычислений для самих функций (Раннее время начала задач -> Позднее время начала задач-> Резервное время на задачи -> Критический путь, сделать так, чтобы программа была не зависима от порядка вычислений
 
         System.out.println("Раннее время начала задач:");
-        for (var n : forwardElevationOrder) {
-            n.calcEarlyStartTime();
-        }
+    //    while (!Node.isAllCalcEarlyStartTime(forwardElevationOrder)) {
+            for (var n : forwardElevationOrder) {
+                n.calcEarlyStartTime();
+            }
+    //    }
 
         System.out.println("Позднее время начала задач:");
         var reverseElevationOrder = new ArrayList<>(forwardElevationOrder);
@@ -117,10 +149,14 @@ public class Main {
          */
         private int earlyStartTime = 0;
 
+        private boolean isCalculatedEarlyStartTime = false;//нужно для того, чтобы ноды которые не были расчитаны не попадали в расчет
+
         /**
          * Позднее время начала.
          */
         private int lateStartTime = 0;
+
+        private boolean isCalculatedLateStartTime = false;//нужно для того, чтобы ноды которые не были расчитаны не попадали в расчет
 
         /**
          * Резерв времени.
@@ -133,11 +169,17 @@ public class Main {
         }
 
         public static Node createInitialNode() {
-            return new Node(0);
+            Node n = new Node(0);
+            //n.isCalculatedEarlyStartTime = true;
+            //n.isCalculatedLateStartTime = true;
+            return n;
         }
 
         public static Node createFinishNode() {
-            return new Node(0);
+            Node n = new Node(0);
+            //n.isCalculatedEarlyStartTime = true;
+            //n.isCalculatedLateStartTime = true;
+            return n;
         }
 
         public Node addPre(Node... preNodes) {
@@ -154,18 +196,42 @@ public class Main {
         }
 
         public void calcEarlyStartTime() {
+            if (isCalculatedEarlyStartTime) {
+                return;
+            }
+/*
+            if (Arrays.stream(preNodes).anyMatch(n -> !(n.isCalculatedEarlyStartTime))) {
+                System.out.print("№" + number + " Пропуск, что-то не посчитано");
+                return;
+            }
+*/
             //System.out.print("Раннее время начала задачи №" + number + " Max(");
             System.out.print("№" + number + " Max(");
             earlyStartTime = Arrays.stream(preNodes)
                     .peek(p -> System.out.print("[" + p.number + "] " + p.earlyStartTime + " + " + p.cost))
-                    .map(p -> p.earlyStartTime + p.cost)
+                    .map(p -> p.getEarlyStartTime() + p.cost)
                     .peek(e -> System.out.print(" = " + e + "; "))
                     .max(Integer::compareTo)
                     .orElse(0);
             System.out.println(") = " + earlyStartTime + "");
-        }
 
+            isCalculatedEarlyStartTime = true;
+        }
+/*
+        public static boolean isAllCalcEarlyStartTime(List<Node> forwardElevationOrder) {
+            return forwardElevationOrder.stream().anyMatch(n -> n.isCalculatedEarlyStartTime);
+        }
+*/
         public void calcLateStartTime() {
+            if (isCalculatedLateStartTime) {
+                return;
+            }
+/*
+            if (Arrays.stream(nextNodes).anyMatch(n -> !(n.isCalculatedLateStartTime))) {
+                System.out.print("№" + number + " Пропуск, что-то не посчитано");
+                return;
+            }
+*/
             //System.out.print("Позднее время начала задачи №" + number + " Max(");
             System.out.print("№" + number + " Min(");
             lateStartTime = Arrays.stream(nextNodes)
@@ -175,6 +241,8 @@ public class Main {
                     .min(Integer::compareTo)
                     .orElse(earlyStartTime);
             System.out.println(") = " + lateStartTime + "");
+
+            isCalculatedLateStartTime = true;
         }
 
         public void calcReserveTime() {
@@ -192,10 +260,18 @@ public class Main {
         }
 
         public int getEarlyStartTime() {
+            if (!isCalculatedEarlyStartTime) {
+                calcEarlyStartTime();
+            }
+
             return earlyStartTime;
         }
 
         public int getLateStartTime() {
+            if (!isCalculatedLateStartTime) {
+                calcLateStartTime();
+            }
+
             return lateStartTime;
         }
 
